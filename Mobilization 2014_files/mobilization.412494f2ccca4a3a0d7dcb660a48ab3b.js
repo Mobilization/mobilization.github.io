e61953e00a60001e92593999a5fd78c45dcb5621
marekdef <marekdef@gmail.com>
2015-02-17 23:16:19 +0100

    Initial commit

---
+++ Mobilization 2014_files/mobilization.412494f2ccca4a3a0d7dcb660a48ab3b.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+  $('p.timeline-description').on('click', function(e){
+    var $this  = $(this);
+    if ($this.hasClass('short')){
+      $this.removeClass('short');
+    } else {
+      $this.addClass('short');
+    }
+  });
+
+  var $agendaTabs = $('.tab-content .tab-pane');
+  $('.modal .speaker-agenda li a').on('click', function(e){
+    var $this  = $(this),
+        agendaItemId = $this.data('agenda'),
+        speakerId = $this.data('speaker'),
+        $agendaItem = $('#agenda-'+agendaItemId),
+        $agendaTab = $agendaItem.closest('.tab-pane'),
+        $agendaTabLabel = $('a[data-target=\"#'+ $agendaTab.attr('id') +'\"]').parent(),
+        $agendaTabOtherLabels = $agendaTabLabel.siblings();
+
+    $agendaTabs.removeClass('active');
+    $agendaTabOtherLabels.removeClass('active');
+    $agendaTab.addClass('active');
+    $agendaTabLabel.addClass('active');
+    $('#speaker_'+speakerId).modal('hide');
+    $('html, body').animate({
+      scrollTop: ($('#agenda-'+agendaItemId).offset().top - 76)
+    }, 1500);
+  });
+});
